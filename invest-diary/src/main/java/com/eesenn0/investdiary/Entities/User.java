53cd07929c22e49d19c05c9f4f094a7327768bc3
eesenn0 <emreesen7@gmail.com>
2023-10-20 19:07:55 +0300

    Added User(entity) class

---
+++ invest-diary/src/main/java/com/eesenn0/investdiary/Entities/User.java
@@ -0,0 +1,20 @@
+package com.eesenn0.investdiary.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="user")
+@Data
+public class User {
+    
+    @Id
+    @GeneratedValue
+    Long id;
+
+    String username;
+    String password;
+}
