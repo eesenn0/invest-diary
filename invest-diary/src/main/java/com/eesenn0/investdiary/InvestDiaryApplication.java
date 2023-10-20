96913c2982e2c3f89c7be11fd130aac9adb96e4c
eesenn0 <emreesen7@gmail.com>
2023-10-20 17:53:34 +0300

    invest-diary

---
+++ invest-diary/src/main/java/com/eesenn0/investdiary/InvestDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.eesenn0.investdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestDiaryApplication.class, args);
+	}
+
+}
---
+++ invest-diary/src/test/java/com/eesenn0/investdiary/InvestDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eesenn0.investdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
