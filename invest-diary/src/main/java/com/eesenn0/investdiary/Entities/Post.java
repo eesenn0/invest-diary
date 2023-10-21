a187bf47adff25e559738aa8dc4332c452b457fd
eesenn0 <emreesen7@gmail.com>
2023-10-21 18:14:26 +0300

    Created the Post entity class

---
+++ invest-diary/src/main/java/com/eesenn0/investdiary/Entities/Post.java
@@ -0,0 +1,36 @@
+package com.eesenn0.investdiary.Entities;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="post")
+@Data
+public class Post {
+    
+    @Id
+    Long id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    User user;
+
+    String title;
+
+    @Lob
+    @Column(columnDefinition = "text")
+    String text;
+}
