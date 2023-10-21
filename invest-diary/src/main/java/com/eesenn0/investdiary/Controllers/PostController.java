6ca9b8c22864aca67cf6cbdd313ad1cebb9dc9c2
eesenn0 <emreesen7@gmail.com>
2023-10-21 21:21:45 +0300

    Created getAllPosts method and PostRepository class

--- invest-diary/src/main/java/com/eesenn0/investdiary/Controllers/PostController.java
+++ invest-diary/src/main/java/com/eesenn0/investdiary/Controllers/PostController.java
@@ -1,7 +1,15 @@
 package com.eesenn0.investdiary.Controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eesenn0.investdiary.Entities.Post;
+import com.eesenn0.investdiary.Services.PostService;
 
 @RestController
 @RequestMapping("/posts")
@@ -11,5 +19,10 @@ public class PostController {
 
     public PostController(PostService postService) {
         this.postService = postService;
+    }
+
+    @GetMapping
+    public List<Post> getAllPosts(@RequestParam Optional<Long> userId) {
+        return postService.getAllPosts(userId);
     }
 }
--- invest-diary/src/main/java/com/eesenn0/investdiary/Services/PostService.java
+++ invest-diary/src/main/java/com/eesenn0/investdiary/Services/PostService.java
@@ -1,5 +1,24 @@
 package com.eesenn0.investdiary.Services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.eesenn0.investdiary.Entities.Post;
+import com.eesenn0.investdiary.Repos.PostRepository;
+
+@Service
 public class PostService {
+
+    private PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+    
+    public List<Post> getAllPosts(Optional<Long> userId) {
+        return null;
+    }
     
 }
